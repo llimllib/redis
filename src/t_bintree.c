f403f4fe3b20c75a98dc35542e524ed762716a00
Bill Mill <bill.mill@gmail.com>
2012-09-01 17:16:26 -0400

    parse the third argument as the score for the node

--- src/t_bintree.c
+++ src/t_bintree.c
@@ -2,9 +2,14 @@
 
 void bsetCommand(redisClient *c) {
     robj *key = c->argv[1];
+    double score=0;
 
     if (c->argc != 4) {
         addReply(c,shared.syntaxerr);
+        return;
+    }
+
+    if (getDoubleFromObjectOrReply(c,c->argv[2],&score,NULL) != REDIS_OK) {
         return;
     }
 
