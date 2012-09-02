8d665a6c87a54fddb27d2d8e5341e58fb16d9d3b
Bill Mill <bill.mill@gmail.com>
2012-09-02 17:32:06 -0400

    Generate a node (but don't attach it yet) and return 1

--- src/t_bintree.c
+++ src/t_bintree.c
@@ -1,16 +1,29 @@
 #include "redis.h"
 
+bintreeNode *bintreeInsert(bintree *tree, double value, robj *member) {
+    bintreeNode *node = zmalloc(sizeof(*node));
+
+    node->value = value;
+    node->obj = member;
+
+    return node;
+    /* TODO: insert node into tree, set left and right pointers */
+}
+
 void bsetCommand(redisClient *c) {
     robj *key = c->argv[1];
     robj *binobj;
-    double score=0;
+    robj *member;
+    bintree *tree;
+    bintreeNode *node;
+    double value=0;
 
     if (c->argc != 4) {
         addReply(c,shared.syntaxerr);
         return;
     }
 
-    if (getDoubleFromObjectOrReply(c,c->argv[2],&score,NULL) != REDIS_OK) {
+    if (getDoubleFromObjectOrReply(c,c->argv[2],&value,NULL) != REDIS_OK) {
         return;
     }
 
@@ -25,5 +38,9 @@ void bsetCommand(redisClient *c) {
         }
     }
 
-    addReply(c,shared.ok);
+    tree = (bintree*)binobj->ptr;
+    member = tryObjectEncoding(c->argv[3]);
+    node = bintreeInsert(tree,value,member);
+
+    addReplyLongLong(c,1);
 }
