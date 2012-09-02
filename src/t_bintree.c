d6db12c0b81c7db9c2ee8b890c612312d59e9e04
Bill Mill <bill.mill@gmail.com>
2012-09-02 09:38:02 -0400

    checking for binobj on bset and creating it if it doesn't exist

--- src/t_bintree.c
+++ src/t_bintree.c
@@ -2,6 +2,7 @@
 
 void bsetCommand(redisClient *c) {
     robj *key = c->argv[1];
+    robj *binobj;
     double score=0;
 
     if (c->argc != 4) {
@@ -11,6 +12,17 @@ void bsetCommand(redisClient *c) {
 
     if (getDoubleFromObjectOrReply(c,c->argv[2],&score,NULL) != REDIS_OK) {
         return;
+    }
+
+    binobj = lookupKeyWrite(c->db,key);
+    if (binobj == NULL) {
+        binobj = createBintreeObject();
+        dbAdd(c->db,key,binobj);
+    } else {
+        if (binobj->type != REDIS_BINTREE) {
+            addReply(c,shared.wrongtypeerr);
+            return;
+        }
     }
 
     addReply(c,shared.ok);
